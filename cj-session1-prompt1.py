94fdeae99a39e38f5bd295af5276e5fbc3e97da3
EliasAlvarez40 <156133502+EliasAlvarez40@users.noreply.github.com>
2024-01-16 14:15:39 -0800

    Session 1 prompt for coding journal 1

---
+++ cj-session1-prompt1.py
@@ -0,0 +1,7 @@
+
+n = "\nElias Alvarez\n \n"
+p = "He/Him\n \n"
+m = "Movie of choice:\nAlthought I do not have a single favoite movie, if I had to chose I would pick 2014's Interstellar. It follow's a retired astronaut turned farmer, as he is recruited into a secret space mission where his's team with finding a suitable replacement for earth. It is by far one of the best science fiction films ever created.\n \n"
+f = "Favoite Food:\nUndoubtedly my favoite food is a burrito. More specifically a California Burrito. It's just like a normal burrito except it has avocado, sour cream, and potatos added.\n"
+
+print(n + p + m + f)
