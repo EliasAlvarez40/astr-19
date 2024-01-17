058577ba1d1b9cc1311ab60a14e7a06c5d1aa2a8
EliasAlvarez40 <156133502+EliasAlvarez40@users.noreply.github.com>
2024-01-16 18:54:04 -0800

    Session 2 for Coding Journal 1

---
+++ cj1-session2.py
@@ -0,0 +1,24 @@
+a = 5.63	#defining two veriables as floating point numbers
+b = 3.48
+
+c = (a + b)	#The sum of the two floating point numbers
+
+print(c)		#Printing the result of the c
+print(type(c))	#Printing c's class type
+
+
+x = 4 		#Defined the two integers
+y = 7
+
+d = (x - y)	#The difference between the two integers
+
+print(d)		#Printing the result of the d
+print(type(d))	#Printing c's class type
+
+
+#multipling a floating point number with an integer
+f = (a*x) 	
+
+print(f)
+print(type(f))
+			
